05f6e18d7a82f4c7eab1ba964a7f7819502c52eb
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-06-06 17:57:04 +0530

    Add project files.

---
+++ TodoListAz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoListAz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
